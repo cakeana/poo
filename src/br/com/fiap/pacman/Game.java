1b3de5670507553db36c5797a39dea45dca76fc6
Ana <bivianana@gmail.com>
2022-09-23 14:01:18 -0300

    ado1

--- src/br/com/fiap/pacman/Game.java
+++ src/br/com/fiap/pacman/Game.java
@@ -8,16 +8,17 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Game extends JFrame implements KeyListener {
 
 	private static final long serialVersionUID = 1L;
 	private Player player = new Player(50, 50, 180);
-	private Ghost ghost1 = new Ghost(0,0,0);
-	private Ghost ghost2 = new Ghost(500,0,0);
-	private Ghost ghost3 = new Ghost(0,500,0);
-	private Ghost ghost4 = new Ghost(500,500,0);
-	private Bomb bomb = new Bomb(100,100);
+	private Ghost ghost1 = new Ghost(0, 0, 0);
+	private Ghost ghost2 = new Ghost(500, 0, 0);
+	private Ghost ghost3 = new Ghost(0, 500, 0);
+	private Ghost ghost4 = new Ghost(500, 500, 0);
+	private Bomb bomb = new Bomb(100, 100);
 	private Booster booster = new Booster(400, 400);
 
 	private JLabel imgPlayer = new JLabel(new ImageIcon("src/images/pacman.png"));
@@ -30,7 +31,8 @@ public class Game extends JFrame implements KeyListener {
 
 	private final int SCREENSIZE = 600;
 	private int speed = 50;
-	
+	private static int i = 0;
+
 	public static void main(String[] args) {
 		new Game().init();
 	}
@@ -44,7 +46,7 @@ public class Game extends JFrame implements KeyListener {
 		ghost2.setScreenSize(SCREENSIZE);
 		ghost3.setScreenSize(SCREENSIZE);
 		ghost4.setScreenSize(SCREENSIZE);
-		
+
 		add(imgPlayer);
 		add(imgGhost1);
 		add(imgGhost2);
@@ -63,7 +65,7 @@ public class Game extends JFrame implements KeyListener {
 	}
 
 	private void render() {
-		
+
 		updateLocation(imgPlayer, player);
 		updateLocation(imgGhost1, ghost1);
 		updateLocation(imgGhost2, ghost2);
@@ -79,41 +81,84 @@ public class Game extends JFrame implements KeyListener {
 	private void updateLocation(JLabel label, GameObject object) {
 		label.setBounds(object.getX(), object.getY(), 50, 50);
 		ImageIcon myImage = (ImageIcon) label.getIcon();
-        Image img = myImage.getImage();
-        Image newImg = img.getScaledInstance(label.getWidth(), label.getHeight(),Image.SCALE_SMOOTH);
-        label.setIcon( new ImageIcon(newImg) );
+		Image img = myImage.getImage();
+		Image newImg = img.getScaledInstance(label.getWidth(), label.getHeight(), Image.SCALE_SMOOTH);
+		label.setIcon(new ImageIcon(newImg));
 	}
 
 	private void run() {
 		while (player.getLife() > 0) {
-			
-			//coloque aqui os métodos de movimentação e colisão 
-			
+			player.mover();
+			ghost1.mover();
+			ghost2.mover();
+			ghost3.mover();
+			ghost4.mover();
+			Integer p = player.getX() + player.getY();
+			Integer g1 = ghost1.getX() + ghost1.getY();
+			Integer g2 = ghost2.getX() + ghost2.getY();
+			Integer g3 = ghost3.getX() + ghost3.getY();
+			Integer g4 = ghost4.getX() + ghost4.getY();
+			Integer b = booster.getX() + booster.getY();
+			Integer bb = bomb.getX() + bomb.getY();
+
+			if (p.equals(g1) || p.equals(g2) || p.equals(g3) || p.equals(g4)) {
+
+				if (player.isInvincible()) {
+					if (i == 4) {
+						player.setInvincible(false);
+					}
+					i++;
+					if (i == 5) {
+						i = 0;
+					}
+				} else {
+					removeVida();
+				}
+
+			}
+
+			if (p.equals(b)) {
+				player.setInvincible(true);
+			}
+
+			if(p.equals(bb)){
+				removeVida();
+			}
+
 			try {
 				Thread.sleep(speed);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
 			render();
-			
+
 		}
+	}
+
+	private void removeVida() {
+		Integer vida = player.getLife() - 1;
+		player.setLife(vida);
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		char c = e.getKeyChar();
-		if (c == '8' || c == 'w') player.setDirection(0);	
-		if (c == '6' || c == 'd') player.setDirection(90);	
-		if (c == '2' || c == 's') player.setDirection(180);	
-		if (c == '4' || c == 'a') player.setDirection(270);	
+		if (c == '8' || c == 'w')
+			player.setDirection(0);
+		if (c == '6' || c == 'd')
+			player.setDirection(90);
+		if (c == '2' || c == 's')
+			player.setDirection(180);
+		if (c == '4' || c == 'a')
+			player.setDirection(270);
 	}
 
 	@Override
-	public void keyPressed(KeyEvent e) {}
+	public void keyPressed(KeyEvent e) {
+	}
 
 	@Override
-	public void keyReleased(KeyEvent e) {}
-	
+	public void keyReleased(KeyEvent e) {
+	}
 
-	
 }
